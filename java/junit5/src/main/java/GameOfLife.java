342a28a9e931ee4996d2bca790d8431c5dbcda4a
Kidlike <steve.tcmg@gmail.com>
2018-11-17 14:10:29 +0100

    code-retreat-setup

---
+++ java/junit5/src/main/java/GameOfLife.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class GameOfLife {
+    public List<Cell> nextIteration(List<Cell> initialState) {
+        List<Cell> nextState = null;
+        
+        return nextState;
+    }
+}
---
+++ java/junit5/src/test/java/GameOfLifeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeTest {
+
+    private GameOfLife gameOfLife;
+
+    @Test
+    public void test_singleCell_dies() {
+
+    }
+}
