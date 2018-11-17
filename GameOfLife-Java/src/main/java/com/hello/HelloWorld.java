3fe8319557b6ea99f7642e0ad6793aec40490900
Kidlike <steve.tcmg@gmail.com>
2018-11-17 14:41:54 +0100

    session-4/initial

--- GameOfLife-Java/src/main/java/com/hello/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.hello;
-
-public class HelloWorld {
-    public String sayIt() {
-        return "Hello World";
-    }
-}
---
+++ GameOfLife-Java/src/test/java/com/hello/CellTest.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CellTest {
+
+    @Test
+    public void test_is_dead() {
+
+    }
+}
